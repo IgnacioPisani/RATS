af3de86f46bd37a6436e05957b8b0acf45f58a39
IgnacioPisani <ignaciopisani12@gmail.com>
2026-06-26 00:53:09 -0300

    commit

--- Source/Game3d/Private/EnemyMixto.cpp
+++ Source/Game3d/Private/EnemyMixto.cpp
@@ -3,7 +3,10 @@
 
 #include "EnemyMixto.h"
 
+#include "EnemyDistance.h"
+#include "HealthComponent.h"
 #include "Components/BoxComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
 
@@ -24,7 +27,16 @@ AEnemyMixto::AEnemyMixto()
 void AEnemyMixto::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (HealthComponent)
+	{
+		HealthComponent->OnLifeChanged.AddDynamic(this, &AEnemyMixto::HandleLifeChanged);
+	}
 	
+	if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
+	{
+		BaseWalkSpeed = MoveComp->MaxWalkSpeed;
+	}
 }
 
 // Called every frame
@@ -101,6 +113,77 @@ void AEnemyMixto::ActivateMeleeHitBox()
 		false);
 }
 
+void AEnemyMixto::HandleLifeChanged(float NewHealth, float NewMaxHealth)
+{
+	Super::HandleLifeChanged(NewHealth, NewMaxHealth);
+	if (NewMaxHealth <= 0.f) return;
+
+	const float Ratio = NewHealth / NewMaxHealth;
+
+	if (Ratio <= 0.6666f && ThirdsTriggered < 1)
+	{
+		ThirdsTriggered = 1;
+		TrySummonDistanceAllies(2);
+		ApplyEnrageSpeed(Tier1SpeedMultiplier);
+	}
+
+	if (Ratio <= 0.3333f && ThirdsTriggered < 2)
+	{
+		ThirdsTriggered = 2;
+		TrySummonDistanceAllies(3);
+		ApplyEnrageSpeed(Tier2SpeedMultiplier);
+	}
+}
+
+void AEnemyMixto::ApplyEnrageSpeed(float SpeedMultiplier)
+{
+	if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
+	{
+		MoveComp->MaxWalkSpeed = BaseWalkSpeed * SpeedMultiplier;
+	}
+}
+
+void AEnemyMixto::TrySummonDistanceAllies(int32 Count)
+{
+	if (!DistanceEnemyClass) return;
+	if (!GetWorld()) return;
+
+	// Limpiar referencias muertas/inválidas antes de chequear el límite
+	SummonedAllies.RemoveAll([](AEnemyDistance* Ally)
+	{
+		return !IsValid(Ally);
+	});
+
+	for (int32 i = 0; i < Count; ++i)
+	{
+		if (SummonedAllies.Num() >= MaxSummonedAllies) break;
+
+		// Ángulo random independiente por cada invocado
+		const float RandomAngle = FMath::FRandRange(0.f, 2.f * PI);
+		const FVector Offset = FVector(
+			FMath::Cos(RandomAngle) * SummonSpawnRadius,
+			FMath::Sin(RandomAngle) * SummonSpawnRadius,
+			0.f);
+
+		FVector SpawnLocation = GetActorLocation() + Offset;
+		FRotator SpawnRotation = GetActorRotation();
+
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.Owner = this;
+		SpawnParams.SpawnCollisionHandlingOverride =
+			ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+		AEnemyDistance* NewAlly = GetWorld()->SpawnActor<AEnemyDistance>(
+			DistanceEnemyClass, SpawnLocation, SpawnRotation, SpawnParams);
+
+		if (NewAlly)
+		{
+			SummonedAllies.Add(NewAlly);
+			UE_LOG(LogTemp, Log, TEXT("AEnemyMixto: Invocó EnemyDistance (%d/%d activos)"),
+				SummonedAllies.Num(), MaxSummonedAllies);
+		}
+	}
+}
 void AEnemyMixto::DeactivateMeleeHitBox()
 {
 	if (!MeleeHitBox) return;
